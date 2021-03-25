107dfc0362353842a378fa787ab55e3a9ab396af
etiennezastko <etiennezastko@hotmail.fr>
2021-03-25 16:01:43 +0100

    test de la grille de bootstrap

---
+++ grid.php
@@ -0,0 +1,73 @@
+<?php include "header.php" ; ?>
+
+<div class="container-fluid"> 
+    <div class="row">
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+    </div>
+    <div class="row">
+        <div class="col-md-4 col-sm-12 border-et">Karina</div>
+        <div class="col-md-4 col-sm-12 border-et">Sonia</div>
+        <div class="col-md-4 col-sm-12 border-et">Romain</div>
+    </div>
+    <div class="row">
+        <div class="col-md-2 col-sm-12 border-et">Etienne</div>
+        <div class="col-md-8 col-sm-12 border-et">Adam</div>
+        <div class="col-md-2 col-sm-12 border-et">Adrien</div>
+    </div> 
+</div>
+
+<div class="container"> 
+    <div class="row">
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+    </div>
+    <div class="row">
+        <div class="col-md-4 col-sm-12 border-et">Karina</div>
+        <div class="col-md-4 col-sm-12 border-et">Sonia</div>
+        <div class="col-md-4 col-sm-12 border-et">Romain</div>
+    </div>
+    <div class="row">
+        <div class="col-md-4 col-sm-12 border-et">Etienne</div>
+        <div class="col-md-8 col-sm-12 border-et">
+        <div class="row">
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+        <div class="col-md-1 col-sm-12 border-et">col-1</div>
+    </div>
+        </div>
+   
+    </div> 
+</div>
+
+<?php include "footer.php" ; ?>
+
---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="site integrer sur la base d'une maquette réalisé par un graphiste">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="shortcut icon" href="logo.png" type="image/png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">   
+</head>
+<body>
