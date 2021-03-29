a6f9475d812af6dfc505b1cc0b6c1f3ced28d0b3
etiennezastko <etiennezastko@hotmail.fr>
2021-03-29 10:45:06 +0200

    integration section 1, 2 & 3

--- header.php
+++ header.php
@@ -1,52 +1,57 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="site integrer sur la base d'une maquette réalisé par un graphiste">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="shortcut icon" href="logo.png" type="image/png">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">   
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="description" content="site integrer sur la base d'une maquette réalisé par un graphiste">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="shortcut icon" href="logo.png" type="image/png">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    <main class="container">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-        <img src="images/weiss-logo.png" alt="logo weiss">
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+  <main class="container">
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src="images/image-weiss-version-desktop/weiss-logo.png" alt="logo weiss">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li>
+                  <hr class="dropdown-divider">
+                </li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li>
           </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <div class="d-flex">
-        <button id="btn-kontakt" class="btn btn-outline-success" type="submit">Search</button>
+          <div class="d-flex">
+            <button id="btn-kontakt" class="btn btn-outline-success" type="submit">Search</button>
+
+
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</nav>
-    
+    </nav>
--- index.php
+++ index.php
@@ -1,3 +1,85 @@
-<?php include "header.php" ; ?>
-<?php include "footer.php" ; ?>
+<?php include "header.php"; ?>
 
+<!--  -->
+<!-- Première section -->
+<div class="card mb-3" style="max-width: 1296px;">
+    <div class="row g-0">
+        <div class="col-md-6">
+            <div class="card-body">
+                <p class="subtitle">INGENIEUR DIETER WEISS </p>
+
+                <h2 class="card-title">
+                    Ingenieurbûro fûr Bau- und Vermessungswesen</h2>
+
+                <p class="card-text-1">Wenn Sie zuverlâssige Ingenieure brauchen- das
+                    Ingenieurbûro Dieter Weiss ist Garant fûr den Erlog Ihrer
+                    Projekte</p>
+
+                <button class="btn-btn-primary" type="submit">Ihr Projekt mituns</button>
+            </div>
+        </div>
+        <div class="col-md-6 image-align">
+
+            <img src="images/image-weiss-version-desktop/image-1.png" alt="image-1.png">
+
+        </div>
+    </div>
+</div>
+
+<!-- Deuxième section -->
+<div class="card mb-3" style="max-width: 1296px;">
+    <div class="row g-0">
+        <div class="col-md-6">
+            <div class="card-body">
+                <p class="subtitle">INGENIEUR DIETER WEISS </p>
+
+                <h2 class="card-title">
+                    Ingenieurbûro fûr Bau- und Vermessungswesen</h2>
+
+                <p class="card-text-1">Wenn Sie zuverlâssige Ingenieure brauchen- das
+                    Ingenieurbûro Dieter Weiss ist Garant fûr den Erlog Ihrer
+                    Projekte</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Troisième section -->
+
+<div class="row">
+    <div class="col-sm-12 col-md-4">
+        <div class="card">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-12 col-md-4">
+        <div class="card">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-12 col-md-4">
+        <div class="card">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+<!-- </div>
+</div> -->
+<?php include "footer.php"; ?>
