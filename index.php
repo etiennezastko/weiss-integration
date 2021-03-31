3f2831d4f81a776e81fc66a7db3b92082141148e
etiennezastko <etiennezastko@hotmail.fr>
2021-03-31 16:31:47 +0200

    update 2

--- index.php
+++ index.php
@@ -2,23 +2,23 @@
 
 <!--  -->
 <!-- Première section -->
-<div class="card mb-3" style="max-width: 1296px;">
-    <div class="row g-0">
-        <div class="col-md-6">
-            <div class="card-body">
-                <p class="subtitle">INGENIEUR DIETER WEISS </p>
+<div class="card mb-3 guideline" style="max-width: 1296px;">
+    <div class="row g-0 guideline">
+        <div class="col-md-6 guideline">
+            <div class="card-body guideline">
+                <p class="subtitle guideline">INGENIEUR DIETER WEISS </p>
 
-                <h2 class="card-title">
+                <h2 class="card-title guideline">
                     Ingenieurbûro fûr Bau- und Vermessungswesen</h2>
 
-                <p class="card-text-1">Wenn Sie zuverlâssige Ingenieure brauchen- das
+                <p class="card-text-1 guideline">Wenn Sie zuverlâssige Ingenieure brauchen- das
                     Ingenieurbûro Dieter Weiss ist Garant fûr den Erlog Ihrer
                     Projekte</p>
 
                 <button class="btn-btn-primary" type="submit">Ihr Projekt mituns</button>
             </div>
         </div>
-        <div class="col-md-6 image-align">
+        <div class="col-md-6 image-align guideline">
 
             <img src="images/image-weiss-version-desktop/image-1.png" alt="image-1.png">
 
@@ -27,14 +27,14 @@
 </div>
 
 <!-- Deuxième section -->
-<div class="card mb-3" style="max-width: 1296px;">
+<div class="card mb-3 guideline" style="max-width: 1296px;">
     <div class="row g-0">
         <div class="col-md-6">
-            <div class="card-body">
-                <p class="subtitle">INGENIEUR DIETER WEISS </p>
-                <h2 class="card-title">
+            <div class="card-body guideline">
+                <p class="subtitle guideline">INGENIEUR DIETER WEISS </p>
+                <h2 class="card-title guideline">
                     Ingenieurbûro fûr Bau- und Vermessungswesen</h2>
-                <p class="card-text-1">Wenn Sie zuverlâssige Ingenieure brauchen- das
+                <p class="card-text-1 guideline">Wenn Sie zuverlâssige Ingenieure brauchen- das
                     Ingenieurbûro Dieter Weiss ist Garant fûr den Erlog Ihrer
                     Projekte</p>
 
@@ -46,8 +46,8 @@
 
 <!-- Troisième section -->
 
-<div class="row">
-    <div class="col-sm-12 col-md-4">
+<div class="row guideline">
+    <div class="col-sm-12 col-md-4 guideline">
         <div class="card">
             <img src="images/image-weiss-version-desktop/article-1.png" class="article-1.png" alt="...">
             <div class="card-body">
@@ -57,7 +57,7 @@
             </div>
         </div>
     </div>
-    <div class="col-sm-12 col-md-4">
+    <div class="col-sm-12 col-md-4 guideline">
         <div class="card">
             <img src="images/image-weiss-version-desktop/article-2.png" class="article-2.png" alt="articles-2">
             <div class="card-body">
@@ -69,8 +69,8 @@
             </div>
         </div>
     </div>
-    <div class="col-sm-12 col-md-4">
-        <div class="card">
+    <div class="col-sm-12 col-md-4 guideline">
+        <div class="card guideline">
             <img src="images/image-weiss-version-desktop/article-3.png" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">Entwurfsvermessun, 3D-Laserscanning</h5>
@@ -82,14 +82,14 @@
 
 
     <!-- Quatrième section -->
-    <div class="col-sm-12 col-md-10">
-        <div class="parent" id="margin-card">
-            <div class="child">
-                <p class="card-subtitle">UNSERE
+    <div class="col-sm-12 col-md-10 guideline">
+        <div class="parent guideline" id="margin-card">
+            <div class="child guideline">
+                <p class="card-subtitle guideline">UNSERE
                     FIRMENHISTORIE</p>
-                <h5 class="card-title-2 child">Diplom-Ingenieur Dieter Weiss grûndete im Jahre 1971 sein Ingenieurnûro.
+                <h5 class="card-title-2 child guideline">Diplom-Ingenieur Dieter Weiss grûndete im Jahre 1971 sein Ingenieurnûro.
                 </h5>
-                <p class="card-text-2">
+                <p class="card-text-2 guideline">
                     Fast 30 Jahre lang fûhrte er es als Alleinninhaber. 1998 trat Sohn Klaus weiss als Stellvertreter an die Seite seines Vaters. Seit 2006 ist Diplom-Ingenieur Klauss Weiss alleiniger Gesvhâftsfûhrer.</p>
                 <img src="images/image-weiss-version-desktop/image-3.png" alt="" class="test">
             </div>
@@ -99,32 +99,61 @@
 
         </div>
         <!--Cinquieme Section -->
-        <div class="card-text-end" style="width: 1296px">
-            <div class="card-body-9 card-align">
-                <div class="margin-card-2">
+        <div class="card-text-end pt-5 pb-5 guideline" style="width: 120%">
+            <div class="card-body-9 card-align d-flex justify-content-between align-items-end">
+
+                <div class="margin-card-2 guideline">
                     <h5 class="card-title">Winssenswertes </h5>
                     <p class="card-text-5">
                         Winssenswertes
                         Integer massa urna senectus maecenas in dapibus urna amet henderitt. In Ipsum, morbi amet aenan. laculis donec sem nullam nunc condimentum nisl non gravida in.
                         UNSERE LETZEN BEITRÄGE
-
                     </p>
                 </div>
-                <div class="btn-bottom"><a href="#" class="btn btn-primary">Go somewhere</a>
+                <div>
+                    <button type="button" class="btn btn-warning" id="section-5__button">Go somewhere</button>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <!--Sixième Section -->
+    <div class="row guideline">
+        <div class="col-sm-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
         </div>
+        <div class="col-sm-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 
 
 
 
-
-
-
-
-
-        <!-- </div>
+    <!-- </div>
 </div> -->
-        <?php include "footer.php"; ?>
+    <?php include "footer.php"; ?>
